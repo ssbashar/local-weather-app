6b50d476618d8dd7d663224680da863e9c76991b
Salma Bashar <salmabashar@gmail.com>
2018-09-18 23:56:17 -0700

    created event and event binding

--- src/app/city-search/city-search.component.ts
+++ src/app/city-search/city-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl, Validators } from '../../../node_modules/@angular/forms';
 import { WeatherService } from '../weather/weather.service';
 import { debounceTime } from 'rxjs/operators';
@@ -11,10 +11,11 @@ import { debounceTime } from 'rxjs/operators';
 export class CitySearchComponent implements OnInit {
   search = new FormControl('', [Validators.minLength(3)])
   
+  @Output() searchEvent = new EventEmitter<string>();
   constructor(private weatherService: WeatherService) { }
 
   getErrorMessage(){
-    return this.search.hasError('minLength') ? 
+    return this.search.hasError('minlength') ? 
     'Type more than 3 characters to search.': '';
   }
 
@@ -23,11 +24,7 @@ export class CitySearchComponent implements OnInit {
     .pipe(debounceTime(1000))
     .subscribe((searchValue: string) => {
       if (!this.search.invalid) {
-        const userInput = searchValue.split(',')
-        .map(s => s.trim());
-        this.weatherService.getCurrentWeather(userInput[0],
-          userInput.length > 1 ? userInput[1] : undefined)
-          .subscribe(data => console.log(data))
+        this.searchEvent.emit(searchValue)
       }
     }
 
